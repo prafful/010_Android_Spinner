8422ddf7bf242716b77704ee6dba4a7ceca31183
prafful <prraful@gmail.com>
2025-02-22 14:34:47 +0530

    progress bar

--- app/src/main/java/com/example/a010_spinner/MainActivity.kt
+++ app/src/main/java/com/example/a010_spinner/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.SeekBar
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +15,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBarC: ProgressBar
+    private lateinit var progressBarH: ProgressBar
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
@@ -25,7 +33,8 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val textView = findViewById<TextView>(R.id.textView)
 
-        val items = arrayOf("No Background", "Beginner", "Learnt once", "Developer 2-3 apps", "Expert")
+        val items =
+            arrayOf("No Background", "Beginner", "Learnt once", "Developer 2-3 apps", "Expert")
         //create array adapter
         //will create an adpater to populate an spinner
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
@@ -35,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         spinner.adapter = adapter
 
         //set listener for the item selected in the spinner
-        spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 textView.text = items[p2]
             }
@@ -46,6 +55,58 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        //get reference to seekbar
+        val seekBar: SeekBar = findViewById(R.id.seekBar)
 
+        //work with seekbar change listener
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                textView.text = "Seekbar level: $p1"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                //yet to implement
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                //yet to implement
+            }
+
+        })
+
+        //get reference to progressbar and button
+        progressBarC = findViewById<ProgressBar>(R.id.progressBar)
+        progressBarH = findViewById<ProgressBar>(R.id.progressBar2)
+        button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            //progressBarC.visibility = View.VISIBLE
+            progressBarH.visibility = View.VISIBLE
+            startProgress()
+        }
+
+
+    }
+
+    private fun startProgress() {
+        //show circular progress bar
+        progressBarC.visibility = View.VISIBLE
+        progressBarH.progress = 0
+
+        //start thread
+        val handler = android.os.Handler()
+        Thread {
+            for (i in 1..100) {
+                handler.post {
+                    progressBarH.progress = i
+                }
+                Thread.sleep(200)
+            }
+            handler.post {
+                progressBarC.visibility = View.GONE
+                progressBarH.progress = 0
+            }
+
+        }.start()
     }
 }
